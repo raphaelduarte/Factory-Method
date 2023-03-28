3f1170b833321e0dda3ee9189cb6551d84122a3d
Raphael Duarte <raphaelduartessph@gmail.com>
2023-03-28 16:16:26 -0300

    Add project files.

---
+++ Factory Method/FactoryMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Method
+{
+    public class FactoryMethod
+    {
+        public IPersonagem Escolher_Personagem(string personagem)
+        {
+            switch(personagem)
+            {
+                case "LiuKang": return new LiuKang();
+                case "Subzero": return new Subzero();
+                case "Scorpion": return new Scorpion();
+                default: return null;
+            }
+        }
+    }
+}
---
+++ Factory Method/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factory_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactoryMethod fm = new FactoryMethod();
+
+            Console.WriteLine("Liu Kang | Subzero | Scorpion");
+            Console.WriteLine();
+            Console.WriteLine("Escolha seu personagem");
+            string escolha = Console.ReadLine();
+
+            IPersonagem personagem = fm.Escolher_Personagem(escolha);
+            Console.WriteLine();
+
+            Console.WriteLine("Voce vai jogar com ");
+            personagem.Escolhido();
+
+            Console.ReadKey();
+        }
+    }  
+}
